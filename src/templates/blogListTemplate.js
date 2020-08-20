182ea3c647ab0c2580bdc4dbc881ac6eca9a8859
Seth Carter <carter.seth.m@gmail.com>
2020-08-20 15:10:30 +0100

    Added pagination links to the blog list template and tweaked image sizes for the same template

--- src/templates/blogListTemplate.js
+++ src/templates/blogListTemplate.js
@@ -22,6 +22,19 @@ const StyledLink = styled(Link)`
     text-decoration: underline;
   }
 `
+const PaginationNumbers = styled.div`
+  .page-button {
+    min-width: 129px;
+  }
+
+  .page-numbers {
+    letter-spacing: 0.3em;
+  }
+
+  .active-page {
+    font-weight: 800;
+  }
+`
 
 const WrapperDiv = styled.div`
   .line-clamp {
@@ -52,51 +65,90 @@ const WrapperDiv = styled.div`
   }
 `
 
-const BlogListTemplate = ({ data }) => (
-  <Layout>
-    <Helmet>
-      <title>The English History Tourist Blog</title>
-      <meta
-        name="description"
-        content="Read the English History Tourist blog for the latest on top history destinations in the UK!"
-      />
-    </Helmet>
-    <Subheader>
-      <h1>Blog</h1>
-      <p>Recommendations, attractions, and top historical sites</p>
-    </Subheader>
-    <WrapperDiv>
-      {data.allContentfulBlogPost.edges.map(edge => (
-        <div key={edge.node.id} className="card mb-3">
-          <StyledLink to={`/blog/${edge.node.slug}`}>
-            <div className="row no-gutters">
-              <div className="col-md-4">
-                <Img
-                  fluid={edge.node.featuredImage.fluid}
-                  className="card-img"
-                  alt={edge.node.title}
-                />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h2 className="card-title">{edge.node.title}</h2>
-                  <p className="card-text line-clamp">
-                    {edge.node.body.json.content[0].content[0].value}
-                  </p>
-                  <p className="card-text">
-                    <small className="text-muted">
-                      {edge.node.publishDate}
-                    </small>
-                  </p>
+const BlogListTemplate = ({ data, pageContext }) => {
+  const { currentPage, numPages } = pageContext
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numPages
+  const prevPage = `/blog/${
+    currentPage - 1 === 1 ? '' : (currentPage - 1).toString()
+  }`
+  const nextPage = `/blog/${(currentPage + 1).toString()}`
+
+  return (
+    <Layout>
+      <Helmet>
+        <title>The English History Tourist Blog</title>
+        <meta
+          name="description"
+          content="Read the English History Tourist blog for the latest on top history destinations in the UK!"
+        />
+      </Helmet>
+      <Subheader>
+        <h1>Blog</h1>
+        <p>Recommendations, attractions, and top historical sites</p>
+      </Subheader>
+      <WrapperDiv>
+        {data.allContentfulBlogPost.edges.map(edge => (
+          <div key={edge.node.id} className="card mb-3">
+            <StyledLink to={`/blog/${edge.node.slug}`}>
+              <div className="row no-gutters">
+                <div className="col-md-4">
+                  <Img
+                    fluid={edge.node.featuredImage.fluid}
+                    className="card-img"
+                    alt={edge.node.title}
+                  />
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h2 className="card-title">{edge.node.title}</h2>
+                    <p className="card-text line-clamp">
+                      {edge.node.body.json.content[0].content[0].value}
+                    </p>
+                    <p className="card-text">
+                      <small className="text-muted">
+                        {edge.node.publishDate}
+                      </small>
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-          </StyledLink>
+            </StyledLink>
+          </div>
+        ))}
+      </WrapperDiv>
+      <PaginationNumbers className="container">
+        <div className="d-flex justify-content-between">
+          <div className="page-button">
+            {!isFirst && (
+              <StyledLink to={prevPage} rel="prev">
+                ← Previous Page
+              </StyledLink>
+            )}
+          </div>
+          <div className="page-numbers">
+            {Array.from({ length: numPages }, (_, i) => (
+              <StyledLink
+                key={`pagination-number${i + 1}`}
+                activeClassName="active-page"
+                to={`/blog/${i === 0 ? '' : i + 1}`}
+              >
+                {i + 1}
+              </StyledLink>
+            ))}
+          </div>
+          <div className="page-button">
+            {!isLast && (
+              <StyledLink to={nextPage} rel="next">
+                Next Page →
+              </StyledLink>
+            )}
+          </div>
         </div>
-      ))}
-    </WrapperDiv>
-  </Layout>
-)
+      </PaginationNumbers>
+    </Layout>
+  )
+}
 
 export default BlogListTemplate
 
@@ -121,7 +173,7 @@ export const query = graphql`
             fluid(
               maxWidth: 658
               resizingBehavior: FILL
-              maxHeight: 370
+              maxHeight: 436
               cropFocus: CENTER
             ) {
               ...GatsbyContentfulFluid
